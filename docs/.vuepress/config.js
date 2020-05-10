fbc7cad22a199bd8afd7602519d283774730406e
gaowei <wgao1241@163.com>
2020-05-10 23:14:24 +0800

    add files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,14 +8,17 @@ module.exports = {
     markdown: {
         lineNumbers: false // 代码块显示行号
     },
-    themeConfig: {
-        nav:[ // 导航栏配置
-            {text: '前端基础', link: '/accumulate/' },
-            {text: '算法题库', link: '/algorithm/'},
-            {text: '微博', link: 'https://baidu.com'}      
-        ],
-        sidebar: 'auto', // 侧边栏配置
-        sidebarDepth: 2, // 侧边栏显示2级
-        logo: '/assets/img/logo.png',
-    }
+    // themeConfig: {
+    //     nav:[ // 导航栏配置
+    //         {text: '前端基础', link: '/accumulate/' },
+    //         {text: '算法题库', link: '/algorithm/'},
+    //         {text: '微博', link: 'https://baidu.com'}      
+    //     ],
+    //     sidebar: 'auto', // 侧边栏配置
+    //     sidebarDepth: 2, // 侧边栏显示2级
+    //     logo: '/assets/img/logo.png',
+    //     sidebar: [
+    //         '/',
+    //     ]
+    // }
 }
