eafa63933763990e29f39687e3e43f6532d82fc6
gaowei2 <gaowei2@niwodai.net>
2020-05-12 16:00:47 +0800

    add layout

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,19 +6,19 @@ module.exports = {
         ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     markdown: {
-        lineNumbers: false // 代码块显示行号
+        lineNumbers: true // 代码块显示行号
     },
-    // themeConfig: {
-    //     nav:[ // 导航栏配置
-    //         {text: '前端基础', link: '/accumulate/' },
-    //         {text: '算法题库', link: '/algorithm/'},
-    //         {text: '微博', link: 'https://baidu.com'}      
-    //     ],
+    themeConfig: {
+        nav:[ // 导航栏配置
+            {text: 'home', link: '/' },
+            {text: 'about', link: '/algorithm/'},
+            {text: 'github', link: 'https://baidu.com'}      
+        ],
     //     sidebar: 'auto', // 侧边栏配置
     //     sidebarDepth: 2, // 侧边栏显示2级
-    //     logo: '/assets/img/logo.png',
-    //     sidebar: [
-    //         '/',
-    //     ]
-    // }
+        logo: '/assets/img/logo.png',
+        sidebar: [
+            '/',
+        ]
+    }
 }
