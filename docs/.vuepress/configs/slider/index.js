c207b91f7ac651acc064b7e4a89e4c4fa20beceb
gaowei2 <gaowei2@niwodai.net>
2020-05-15 15:35:15 +0800

    ad

--- docs/.vuepress/configs/slider/index.js
+++ docs/.vuepress/configs/slider/index.js
@@ -2,18 +2,19 @@ module.exports = {
     '/views/' : [
         '',
         {
+            title:'JavaScript篇',
+            collapsable: true,
+            children: [
+                'javascript/jingdu',
+                'javascript/promise',
+            ]
+        },
+        {
             title:'webpack篇',
             collapsable: true,
             children: [
                 'webpack/tree-sharking',
                 'webpack/plugins',
-            ]
-        },
-        {
-            title:'JavaScript篇',
-            collapsable: true,
-            children: [
-                'javascript/promise',
             ]
         },
     ]
--- docs/views/webpack/plugins.md
+++ docs/views/webpack/plugins.md
@@ -1,1 +1,2 @@
-# plugins
+# plugins解析
+### html-webpack-plugin 将打包后文件插入到index.html中
